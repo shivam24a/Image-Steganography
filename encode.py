d37ae0ab2c55e843f1b09f4a86be924501916fc6
shivam24a <38680429+shivam24a@users.noreply.github.com>
2020-01-03 19:29:43 +0530

    update encode.py
    adding extra feature of password to encode the the code

--- encode.py
+++ encode.py
@@ -1,5 +1,5 @@
 from PIL import Image
-codeword='10000000'
+codeword2='00011110'
 bitlength=8
 total_numbers_in_mode=3;
 image=Image.new('1',(0,0))
@@ -7,18 +7,20 @@ image=Image.new('1',(0,0))
 # imagedata=[[122,222,121],[143,154,145],[144,143,134],[134,144,155],[134,134,133],[154,176,154],[134,134,133],[122,122,121],[144,143,134],[134,144,155],[134,134,133],[122,122,121],[144,143,134],[144,143,134],[134,144,155],[134,134,133],[154,176,154],[134,134,133],[122,122,121],[144,143,134]]
 def Encode():
 	global image
-	image_path=input("Enter Image Address")
+	image_path=input("Enter Image Address to Encode : ")
+	codeword1=input("Set Encode Password for the Image : ")
 	image=Image.open(image_path)
 	imagedata= list(image.getdata())
 	# h,w=image.size()
 
-	string_to_encode=input("enter string")
+	string_to_encode=input("Enter text to Encode : ")
+	string_to_encode=codeword1+chr(int(codeword2,2))+string_to_encode
 	binary_coversion_string=''
 	for element in string_to_encode:
 		binary_coversion_string+=format(ord(element),'08b')
 	# s='111010011010'
 	# print(s," !st")
-	string_to_encode=codeword + binary_coversion_string + codeword
+	string_to_encode=binary_coversion_string + codeword2
 	# print(string_to_encode," !st")
 	while len(string_to_encode)%total_numbers_in_mode:
 		string_to_encode+='0'
@@ -45,5 +47,6 @@ def Encode():
 		for x in range(image.size[0]):
 			newimage.putpixel((x,y),tuple(imagedata[pixel_counter]))
 			pixel_counter+=1
-	encoded_save_address=input("Enter address to save ")
+	encoded_save_address=input("Enter Address to save Image file : ")
 	newimage.save(encoded_save_address)
+	print("File saved")
