0b393772f4538a855fcdaefecefec5e839d4eae9
shivam24a <38680429+shivam24a@users.noreply.github.com>
2020-01-03 19:25:29 +0530

    update app.py
    making content more systematic and adding option to use this app again
    also adding extra features for password

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ from PIL import Image
 import encode
 import decode
 
-k=int(input("Enter 1 for encode \n 2 for decode \n other key to exit\n"))
-if k==1 :
-	encode.Encode()
-if k==2 :
-	decode.Decode()
+conti=1
+while  conti:
+	k=input("Press Key 1 : Encode \n          2 : Decode \n  Other key : Exit\n")
+	if k=='1' :
+		encode.Encode()
+	if k=='2' :
+		decode.Decode()
+	conti=input("Want to Encode/Decode again(y/n) : ");
+	if conti=="y" or conti=="Y" or conti=="yes" or conti=="YES" or conti=="Yes":
+		conti=1
+	else :
+		conti=0
