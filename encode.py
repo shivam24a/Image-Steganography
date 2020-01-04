c98e43c0680a3570a1286d791445aeceefd555f4
shivam24a <38680429+shivam24a@users.noreply.github.com>
2020-01-05 01:09:54 +0530

    update encode.py

--- encode.py
+++ encode.py
@@ -1,52 +1,69 @@
 from PIL import Image
 codeword2='00011110'
 bitlength=8
-total_numbers_in_mode=3;
+total_numbers_in_mode=3
+RGB_mode_number=3
 image=Image.new('1',(0,0))
 
 # imagedata=[[122,222,121],[143,154,145],[144,143,134],[134,144,155],[134,134,133],[154,176,154],[134,134,133],[122,122,121],[144,143,134],[134,144,155],[134,134,133],[122,122,121],[144,143,134],[144,143,134],[134,144,155],[134,134,133],[154,176,154],[134,134,133],[122,122,121],[144,143,134]]
+
 def Encode():
+
 	global image
+	global total_numbers_in_mode
 	image_path=input("Enter Image Address to Encode : ")
 	codeword1=input("Set Encode Password for the Image : ")
 	image=Image.open(image_path)
 	imagedata= list(image.getdata())
-	# h,w=image.size()
+	image.close()
+
+	image2=Image.open(image_path)
+	image2=image2.resize((int(image.size[0]/2),int(image.size[1]/2)))
+	image2.show()
+	image2.close()
+	total_numbers_in_mode=len(imagedata[0])
+	# element_counter_in_sublist=0
 
 	string_to_encode=input("Enter text to Encode : ")
 	string_to_encode=codeword1+chr(int(codeword2,2))+string_to_encode
 	binary_coversion_string=''
+
 	for element in string_to_encode:
 		binary_coversion_string+=format(ord(element),'08b')
-	# s='111010011010'
-	# print(s," !st")
 	string_to_encode=binary_coversion_string + codeword2
-	# print(string_to_encode," !st")
-	while len(string_to_encode)%total_numbers_in_mode:
+
+	while len(string_to_encode)%RGB_mode_number:
 		string_to_encode+='0'
 	element_counter=0
 	imagedata_counter=0
 	newlist=[]
+	
 	#encodetion
+	# print(imagedata[:9])
 	for sublist in imagedata:
-		for element in sublist:
+		for element_counter_in_sublist in range(3):
+			element=sublist[element_counter_in_sublist]
 			if element%2 !=int(string_to_encode[element_counter],2):
 				element-=1;
-			newlist.append(element)
+			newlist.append(element)			# newlist[element_counter_in_sublist]=element
 			element_counter+=1;
+		if total_numbers_in_mode==4:
+			newlist.append(sublist[3])		# newlist[3]=sublist[3]
 		imagedata[imagedata_counter]=newlist
+		# print(newlist)
 		newlist=[]
 		imagedata_counter+=1
 		if element_counter>=len(string_to_encode):
 			break
-	# print(imagedata)
 
+	# print(imagedata)
 	newimage=Image.new(image.mode,image.size)
 	pixel_counter=0
 	for y in range(image.size[1]):
 		for x in range(image.size[0]):
 			newimage.putpixel((x,y),tuple(imagedata[pixel_counter]))
 			pixel_counter+=1
+	
 	encoded_save_address=input("Enter Address to save Image file : ")
 	newimage.save(encoded_save_address)
 	print("File saved")
